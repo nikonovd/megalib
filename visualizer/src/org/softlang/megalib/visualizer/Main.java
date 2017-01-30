6a78a4e6b9dede609c77326619b10d1e1dcc25e6
Dmitri Nikonov <nikonov.dmitri@gmail.com>
2017-01-30 15:28:07 +0100

    Minor change to checker groupId in pom.xml. Introducing new project visualizer

---
+++ visualizer/src/org/softlang/megalib/visualizer/Main.java
@@ -0,0 +1,16 @@
+/*
+ *  All rights reserved.
+ */
+package org.softlang.megalib.visualizer;
+
+/**
+ *
+ * @author Dmitri Nikonov <dnikonov at uni-koblenz.de>
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        System.out.println("Main method");
+    }
+    
+}
