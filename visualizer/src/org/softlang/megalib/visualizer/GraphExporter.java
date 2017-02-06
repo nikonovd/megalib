6dc06b54a77206e07fa2da062dfe15d2deef0c2b
Guilherme Fial <gfial@uni-koblenz.de>
2017-02-07 00:31:24 +0100

    Simply created a class for the convertion of megamodel information into the different graph languages

---
+++ visualizer/src/org/softlang/megalib/visualizer/GraphExporter.java
@@ -0,0 +1,16 @@
+package org.softlang.megalib.visualizer;
+
+import org.java.megalib.models.MegaModel;
+import org.softlang.megalib.visualizer.utils.VisualizerType;
+
+public class GraphExporter {
+	
+	/**
+	 * Converts the data found in the MegaModel into graph language of the VisualizerType.
+	 * @param model The MegaModel
+	 * @param type The graph type
+	 */
+	public GraphExporter(MegaModel model, VisualizerType type) {
+		
+	}
+}
