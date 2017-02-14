b7540696e1713316674150c785c4a3197e50d805
Dmitri Nikonov <nikonov.dmitri@gmail.com>
2017-02-14 13:18:36 +0100

    Cleanup

--- visualizer/src/org/softlang/megalib/visualizer/FileVisualizer.java
+++ visualizer/src/org/softlang/megalib/visualizer/FileVisualizer.java
@@ -8,8 +8,6 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import org.softlang.megalib.visualizer.models.Graph;
 
 /**
